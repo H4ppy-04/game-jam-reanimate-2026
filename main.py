11d0adc68b31ae643f2e23174e0c9fb322acac47
joshuarose <joshuarose@gmx.com>
2026-07-06 11:26:00 +1000

    player health bar

--- main.py
+++ main.py
@@ -21,10 +21,38 @@ class GameState(enum.Enum):
     GAME = 1
 
 
+class Healthbar(pygame.sprite.Sprite):
+    def __init__(self, x, y, *groups) -> None:
+        super().__init__(*groups)
+
+        width = 150
+        height = 50
+        self.image = pygame.Surface((width, height))
+        self.image.fill((255, 0, 0))
+
+        self.rect = pygame.rect.Rect(x, y, width, height)
+
+    def draw(self, display):
+        display.blit(self.image, self.rect)
+
 game_state = GameState.MENU
 
 play_text = font.render("PLAY GAME", True, (255, 255, 255))
 mx, my = 0, 0
+
+player_healthbar = Healthbar(10, 10)
+
+def draw_player_health(surface):
+    cell_width = 10
+    spacing = 20 # pixels
+    for i in range(0, (9 * cell_width) * 2, spacing):
+        pygame.draw.rect(surface, (0, 200, 0), pygame.rect.Rect(i, 10, 10, 25))
+
+
+def draw_enemy_health(surface):
+    # just a health bar
+    health_bar_size = 150
+    pygame.draw.rect(surface, (200, 0, 0), pygame.rect.Rect(1920 - health_bar_size - 10, 10)
 
 while True:
     for event in pygame.event.get():
@@ -48,6 +76,11 @@ while True:
     if game_state == game_state.GAME:
         print("actual game screen")
         display.fill((50, 50, 50))
-    
+
+        # draw Healthbar
+        draw_player_health(display)
+
+        # draw bar to have UI stuff blow it
+        pygame.draw.rect(display, (20, 20, 20), pygame.rect.Rect(0, 1080 - 150, 1920, 20))
 
     pygame.display.update()
