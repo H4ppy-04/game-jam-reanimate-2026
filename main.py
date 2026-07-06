14f69c3d1f54d938ee08e63e7644476fe2a39fbe
joshuarose <joshuarose@gmx.com>
2026-07-06 11:11:17 +1000

    actual game screen now

--- main.py
+++ main.py
@@ -24,17 +24,30 @@ class GameState(enum.Enum):
 game_state = GameState.MENU
 
 play_text = font.render("PLAY GAME", True, (255, 255, 255))
+mx, my = 0, 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
+        if event.type == pygame.MOUSEMOTION:
+            mx, my = pygame.mouse.get_pos()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            match game_state:
+                case game_state.MENU:
+                    game_state = game_state.GAME
+
 
     if game_state == game_state.MENU:
         print("game state menu active")
         display.fill((25, 25, 25))
         display.blit(play_text, (1920 / 2 - play_text.width / 2, 1080 / 2 - play_text.height / 2))
+
+    if game_state == game_state.GAME:
+        print("actual game screen")
+        display.fill((50, 50, 50))
     
 
     pygame.display.update()
