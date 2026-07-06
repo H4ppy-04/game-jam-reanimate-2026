9813d1b03db2f9c6ff6a79cc7aa0a95e851268e1
joshuarose <joshuarose@gmx.com>
2026-07-06 11:07:38 +1000

    play button

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import enum
+
 import pygame
 
 pygame.init()
@@ -5,11 +7,34 @@ pygame.init()
 
 display = pygame.display.set_mode((1920, 1080))
 
+font = pygame.font.Font("./assets/Fonts/Kenney Pixel Square.ttf", 50)
+
+
+
+class Dice:
+    def __init__(self) -> None:
+        pass
+
+
+class GameState(enum.Enum):
+    MENU = 0,
+    GAME = 1
+
+
+game_state = GameState.MENU
+
+play_text = font.render("PLAY GAME", True, (255, 255, 255))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
 
-    display.fill((0,0,0))
+    if game_state == game_state.MENU:
+        print("game state menu active")
+        display.fill((25, 25, 25))
+        display.blit(play_text, (1920 / 2 - play_text.width / 2, 1080 / 2 - play_text.height / 2))
+    
+
     pygame.display.update()
